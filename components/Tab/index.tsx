33aa4ffa689c0a54fb159fd15ae473cd6dd605bc
kana.kita <kitagawa@ham.works>
2021-10-14 09:44:55 +0900

    タブボタンをコンポーネント化しないでインラインでHTMLを書くようにした

--- components/Tab/index.tsx
+++ components/Tab/index.tsx
@@ -5,8 +5,8 @@ interface TabProps {
     label: string,
     id: string,
     panel: JSX.Element | JSX.Element[] | string;
-  }[]
-  initialOpen?: string // 初期状態で開いているタブのid
+  }[] // タブの配列
+  initialOpen?: string // 初期状態で開いているタブのid。指定が無い場合は1つ目のタブのidが入る
 }
 
 function Tab({ tabItems, initialOpen = tabItems[0].id }: TabProps) {
@@ -14,7 +14,7 @@ function Tab({ tabItems, initialOpen = tabItems[0].id }: TabProps) {
   console.log(activeItem)
   return (
     <>
-      <TabButton
+      <TabButtonInline
         items={tabItems}
         activeItem={activeItem}
         setActiveItem={setActiveItem}
@@ -27,91 +27,51 @@ function Tab({ tabItems, initialOpen = tabItems[0].id }: TabProps) {
   )
 }
 
-interface TabButtonProps {
-  items: Object[],
-  activeItem: string
-  setActiveItem: (value: string) => void
+interface TabButtonInlineProps {
+  items: {
+    label: string, // ボタンのラベル
+    id: string // ID
+  }[],
+  activeItem: string // 表示状態のタブのid
+  setActiveItem: (value: string) => void // 表示状態のタブのidをセットする関数
 }
-export function TabButton({items, setActiveItem, activeItem}: TabButtonProps) {
-  // function handleClick (event) {
-  //   onHandleClick(event)
-  //   console.log(event)
-  // }
+
+export function TabButtonInline({items, setActiveItem, activeItem}: TabButtonInlineProps) {
+  function handleClick (event: any) {
+    setActiveItem(event.target.getAttribute('data-id'))
+  }
   return (
     <nav>
       <div className="nav nav-tabs" role="tablist">
-        {items.map(({label, id}) => (
-          <TabButtonItem
-            key={id}
-            label={label}
-            id={id}
-            onHandleClick={function () {
-              setActiveItem(id)
-            }}
-            activeItem={activeItem}
-          />
-        )
-        // {
-        //   return (
-        //     <a
-        //       key={id}
-        //       className={`nav-link ${isActive ? 'active' : ''}`}
-        //       id={`nav-tab-${id}`}
-        //       href={`#nav-${id}`}
-        //       aria-controls={`nav-${id}`}
-        //       aria-selected="true"
-        //       data-bs-toggle="tab"
-        //       role="tab"
-        //       onClick={handleClick}
-        //     >
-        //       {label}
-        //     </a>
-        //   )
-        // }
-        )}
+        {items.map(({label, id}) => {
+          return (
+            <a
+              key={id}
+              className={`nav-link ${activeItem === id ? 'active': ''}`}
+              id={`nav-tab-${id}`}
+              href={`#nav-${id}`}
+              aria-controls={`nav-${id}`}
+              aria-selected="true"
+              data-bs-toggle="tab"
+              role="tab"
+              onClick={handleClick}
+              data-id={id}
+            >
+              {label}
+            </a>
+          )
+        })}
       </div>
     </nav>
   )
 }
 
-interface TabButtonItemProps {
-  label: string, // ボタンのラベル
-  id: string, // ボタンのID
-  onHandleClick: (value: string) => void　// ラベルをセットする関数
-  activeItem: string
-}
-
-export function TabButtonItem({label, id, onHandleClick, activeItem}: TabButtonItemProps) {
-
-  /**
-   * ボタンを押した時
-   */
-  function handleClick () {
-    onHandleClick(id)
-    // console.log(id)
-  }
-
-  return (
-    <a
-      key={id}
-      className={`nav-link ${activeItem === id ? 'active': ''}`}
-      id={`nav-tab-${id}`}
-      href={`#nav-${id}`}
-      aria-controls={`nav-${id}`}
-      aria-selected={activeItem === id ? 'true': 'false'}
-      data-bs-toggle="tab"
-      role="tab"
-      onClick={handleClick}
-    >
-      {label}
-    </a>
-  )
-
-}
-
 interface TabContentProps {
-  items: Object[],
-  activeItem: string
+  items: {
+    id: string, // タブのID
+    panel: JSX.Element | JSX.Element[] | string; // タブの中身
+  }[],
+  activeItem: string // 表示状態のタブのid
 }
 export function TabContent({items, activeItem}:TabContentProps) {
   return (
@@ -121,7 +81,7 @@ export function TabContent({items, activeItem}:TabContentProps) {
           <div
             key={id}
             className={`tab-pane fade ${activeItem === id ? 'show active' : ''}`}
-            id={`tab-${id}`}
+            id={id}
             role="tabpanel"
             aria-labelledby={`tab-${id}`}
           >
